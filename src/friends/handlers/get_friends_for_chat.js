21cbff97fe174ac94ac1c8e7f109f4e14f3abf89
Ashish Bhangade <ashishbhangde.linkites@gmail.com>
2020-08-05 21:09:30 +0530

    Change for friend list

--- src/friends/handlers/get_friends_for_chat.js
+++ src/friends/handlers/get_friends_for_chat.js
@@ -4,6 +4,7 @@ import Helpers from '../../helpers'
 import Users from '../../models/users'
 import Friend from '../../models/friends'
 import mongoose from 'mongoose';
+import Chat from '../../models/chat';
 
 let defaults = {}
 /*
@@ -13,24 +14,84 @@ const handler = async (request, reply) => {
   const payload = request.payload
   try {
       const id = await Helpers.extractUserId(request)
+      // const user = await Friend.aggregate([
+      //                   { $match: { requester: mongoose.Types.ObjectId(id), status: 3 }},
+      //                       { "$lookup": {
+      //                           "from": Users.collection.name,
+      //                           let: { recipient :"$recipient"},
+      //                           "pipeline": [
+      //                               { $match: { $expr: { $eq: ["$$recipient", "$_id"] }}},
+      //                               { $project: { firstName: 1, lastName: 1, profileImage: 1, isOnline: 1, "full_name":     {$concat: ['$firstName', ' ', '$lastName']} }}
+      //                           ],
+      //                           "as": "users"
+      //                       }},
+      //                       { "$unwind": "$users" },
+      //                       {
+      //                         "$replaceRoot": {
+      //                         "newRoot": "$users"
+      //                         }
+      //                       },
+      //                   ])
+
+
       const user = await Friend.aggregate([
-                        { $match: { requester: mongoose.Types.ObjectId(id), status: 3 }},
-                            { "$lookup": {
-                                "from": Users.collection.name,
-                                let: { recipient :"$recipient"},
-                                "pipeline": [
-                                    { $match: { $expr: { $eq: ["$$recipient", "$_id"] }}},
-                                    { $project: { firstName: 1, lastName: 1, profileImage: 1, isOnline: 1, "full_name":     {$concat: ['$firstName', ' ', '$lastName']} }}
-                                ],
-                                "as": "users"
-                            }},
-                            { "$unwind": "$users" },
-                            {
-                              "$replaceRoot": {
-                              "newRoot": "$users"
-                              }
-                            },
-                        ])
+       { $match: { requester: mongoose.Types.ObjectId(id), status: 3 } },
+        {
+          $lookup: {
+            from: Chat.collection.name,
+            let: { recipient: "$recipient" },
+            pipeline: [
+              {
+                $match: {
+                  $or: [
+                    {
+                      $and: [
+                        { $expr: { $eq: ["$senderId", "$$recipient"] } },
+                        { $expr: { $eq: ["$receiverId", mongoose.Types.ObjectId(id) ] } },
+                      ],
+                    },
+                    {
+                      $and: [
+                        { $expr: { $eq: ["$senderId", mongoose.Types.ObjectId(id) ] } },
+                        { $expr: { $eq: ["$receiverId", "$$recipient"] } },
+                      ],
+                    },
+                  ],
+                },
+              },
+              { $sort: { createdAt: -1 } },
+              { $limit: 1 },
+            ],
+            as: "lastMessage",
+          },
+        },
+        {
+          $lookup: {
+            from: Users.collection.name,
+            let: { recipient: "$recipient" },
+            pipeline: [
+              { $match: { $expr: { $eq: ["$$recipient", "$_id"] } } },
+              {
+                $project: {
+                  firstName: 1,
+                  lastName: 1,
+                  profileImage: 1,
+                  isOnline: 1,
+                  full_name: { $concat: ["$firstName", " ", "$lastName"] },
+                },
+              },
+            ],
+            as: "users",
+          },
+        },
+        { $unwind: "$users" },
+        { $addFields: { "users.lastMessage": { $arrayElemAt:["$lastMessage.message", 0] }}},
+        {
+          $replaceRoot: {
+            newRoot: "$users",
+          },
+        },
+      ]);
     
     return reply({
       status: true,
