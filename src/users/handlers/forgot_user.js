a71db2347fd2a29b948b3aa88e0a35871c2234a1
Faaiz <iamfaaizali@gmail.com>
2020-10-07 17:18:40 +0530

    forgot password ul change

--- src/users/handlers/forgot_user.js
+++ src/users/handlers/forgot_user.js
@@ -29,7 +29,7 @@ const handler = async (request, reply) => {
       var body={
           email:email,
           // link:`${process.env.WEB_HOST}/updatePassword/`+resetPasswordToken
-          link:`http://localhost:3000/reset/`+resetPasswordToken
+          link:`https://stag.flyfriends.net/reset/`+resetPasswordToken
       }
       await EmailService.sendMail(body,function(err,data){
         console.log('mail sent');
